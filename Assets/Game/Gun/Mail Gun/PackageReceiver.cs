a1d7e4bb17e88306f0d4f146ae1e2ae03ef7cfe4
James Kelly <dxk2294@rit.edu>
2023-04-30 14:46:34 -0700

    I am Morbius.

--- Assets/Game/Gun/Mail Gun/PackageReceiver.cs
+++ Assets/Game/Gun/Mail Gun/PackageReceiver.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,20 @@ using UnityEngine.Events;
 
 public class PackageReceiver : MonoBehaviour
 {
+    public bool GenerateRandomPackageOnAwake = false;
     public bool WantsPackage = false;
     public PackageType PackageType = PackageType.automaton;
 
     public UnityEvent OnReceipt; // Called when they get the package they wanted
+
+    private void Awake()
+    {
+        if (GenerateRandomPackageOnAwake)
+        {
+            var allValues = Enum.GetValues(typeof(PackageType));
+            PackageType = (PackageType) allValues.GetValue(UnityEngine.Random.Range(0, allValues.Length));
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
